a69b04700413c3de6a7d633ff34e4dc3ba367552
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 01:51:41 -0500

    fixed mongodb and mongoose connection

--- myApp.js
+++ myApp.js
@@ -1,10 +1,9 @@
-const db = require('mongodb')
+const mongodb = require('mongodb')
 const mongoose = require('mongoose')
 
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-console.log(process.env.MONGO_URI)
 
 let Person;
 
