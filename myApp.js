4b7de9933343cfd352783f1ab646254f84337d52
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 01:42:07 -0500

    added mongodb and mongoose connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
+const db = require('mongodb')
+const mongoose = require('mongoose')
+
 require('dotenv').config();
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(process.env.MONGO_URI)
 
 let Person;
 
