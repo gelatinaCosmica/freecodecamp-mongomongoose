b39c6f890cf33b0ba75d4e24bc1da3a6140d0634
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 20:14:52 -0500

    set up mongodb and mongoose

--- myApp.js
+++ myApp.js
@@ -1,10 +1,8 @@
-const mongodb = require('mongodb')
 const mongoose = require('mongoose')
-
 require('dotenv').config();
 
-// const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+// mongodb.connect(process.env.file)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
