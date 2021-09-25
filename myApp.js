40f54ca966c55a7408a674e661ce41ca0500c20d
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 20:41:46 -0500

    created Person Scheme

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
+const { Schema } = mongoose
 require('dotenv').config();
 
 // mongodb.connect(process.env.file)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let Person = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: Array
+})
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
