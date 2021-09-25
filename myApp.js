accf3d2fd10ef63196de057c2061c2b858ee3656
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 20:44:07 -0500

    fixed Person Scheme

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 let Person = new Schema({
   name: { type: String, required: true },
   age: Number,
-  favoriteFoods: Array
+  favoriteFoods: [String]
 })
 
 const createAndSavePerson = (done) => {
