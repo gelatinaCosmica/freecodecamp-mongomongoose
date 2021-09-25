bde0924d03d5e9190b0495e8253db6a544560bd6
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 20:56:01 -0500

    added model for personSchema and created new person

--- myApp.js
+++ myApp.js
@@ -1,14 +1,24 @@
 const mongoose = require('mongoose')
-const { Schema } = mongoose
+const { Schema, model } = mongoose
 require('dotenv').config();
 
-// mongodb.connect(process.env.file)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person = new Schema({
+let PersonSchema = new Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String]
+})
+
+let Person = model('Person', PersonSchema)
+
+let personX = new Person({
+  name: 'Toth Albeiro',
+  age: 2,
+  favoriteFoods: [
+    'comida humeda',
+    'pollo',
+  ]
 })
 
 const createAndSavePerson = (done) => {
