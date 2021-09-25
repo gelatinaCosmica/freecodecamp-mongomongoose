82699f97ce4ba1c9b3b02f34c633eeedd94d279a
gelatina <santiagozapatabohorquez@hotmail.com>
2021-09-24 23:08:21 -0500

    findPeopleByName function added

--- myApp.js
+++ myApp.js
@@ -36,7 +36,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find(personName, (err, data) => {
+    if (err) return console.error(err)
+    done(null, data);
+  })
 };
 
 const findOneByFood = (food, done) => {
